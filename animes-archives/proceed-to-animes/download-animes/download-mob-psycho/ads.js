2bc7333b3537e638c9e20c9389e82f19f4576ec3
divine100 <77244292+divine100@users.noreply.github.com>
2022-05-06 00:08:01 +0100

    Add files via upload

---
+++ animes-archives/proceed-to-animes/download-animes/download-mob-psycho/ads.js
@@ -0,0 +1,6 @@
+const player = new OpenPlayerJS('video', {
+  ads: {
+    src: ['https://www.videosprofitnetwork.com/watch.xml?key=80b3047cba01be13079da7975f4e537c', 'https://www.videosprofitnetwork.com/watch.xml?key=80b3047cba01be13079da7975f4e537c',]
+  }
+});
+player.init();
